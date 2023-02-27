26f71dcb558f8cff38243ad22eb7462d1a3be702
David <david.klesch@gmail.com>
2023-02-27 14:48:46 +0300

    picocli + make files added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,32 +12,28 @@ import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
 public class App {
-    @Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
-            description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
-    static
-    class CheckSum implements Callable<Integer> {
+    @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 4.0",
+            description = "Compares two configuration files and shows a difference.")
+    static class GenDiff implements Callable<Integer> {
 
-        @Parameters(index = "0", description = "The file whose checksum to calculate.")
-        private File file;
+        //@Parameters(index = "0", description = "The file whose checksum to calculate.")
+        //private File file;
 
-        @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
-        private String algorithm = "SHA-256";
+        //@Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+        //private String algorithm = "SHA-256";
 
         @Override
         public Integer call() throws Exception { // your business logic goes here...
-            byte[] fileContents = Files.readAllBytes(file.toPath());
-            byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
-            System.out.printf("%0" + (digest.length * 2) + "x%n", new BigInteger(1, digest));
+            //byte[] fileContents = Files.readAllBytes(file.toPath());
+            //byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
+            //System.out.printf("%0" + (digest.length * 2) + "x%n", new BigInteger(1, digest));
             return 0;
         }
-
-        // this example implements Callable, so parsing, error handling and handling user
-        // requests for usage help or version help can be done with one line of code.
 
     }
 
     public static void main(String... args) {
-        int exitCode = new CommandLine(new CheckSum()).execute(args);
+        int exitCode = new CommandLine(new GenDiff()).execute(args);
         System.exit(exitCode);
     }
 
