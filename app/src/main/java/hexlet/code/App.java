5a3a982b439b590687a0a786f76a710f98f9f874
David <david.klesch@gmail.com>
2023-03-06 15:21:57 +0300

    Parser

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,21 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Command(name = "app", mixinStandardHelpOptions = true, version = "app 0.0.1",
         description = "Compare 2 JSONs.")
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
     @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.0.1",
             description = "Compares two configuration files and shows a difference.")
 
@@ -19,7 +24,10 @@ public class App implements Callable<Integer> {
     @Parameters(index = "1", description = "path to second file")
     private String filepath2;
 
-    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            paramLabel = "format",
+            defaultValue = "stylish")
     private String format = "SHA-256";
 
     @Override
@@ -41,5 +49,6 @@ public class App implements Callable<Integer> {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
+
 }
 
