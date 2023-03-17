d4588cdc1d6c7c7982d87f608e2781848edfa48b
Daniil Zatiaev <d.zatiaev@altsoft.spb.ru>
2023-03-17 22:16:07 +0300

    done

--- app/src/test/java/hexlet/DifferTest.java
+++ app/src/test/java/hexlet/DifferTest.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class DifferTest {
+public final class DifferTest {
     private static String resultJson;
     private static String resultPlain;
     private static String resultStylish;
